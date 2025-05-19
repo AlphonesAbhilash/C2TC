d0d8423ebac9831755f85221fda351d4035aa8aa
Alphones <alphonesabhilash@gmail.com>
2025-05-20 00:57:48 +0530

    Comparable and Compare to Practice

--- TNS_JAVA/src/com/tns/collections/EsalCompare.java
+++ TNS_JAVA/src/com/tns/collections/EsalCompare.java
@@ -7,7 +7,7 @@ public class EsalCompare implements Comparator<Employee>{
 	@Override
 	public int compare(Employee e1,Employee e2) {
 		
-		if (e1.sal==e2.sal) {
+		if (e1.sal==e2	.sal) {
 			return 0;
 		}else if(e1.sal>e2.sal) {
 			return 1;
