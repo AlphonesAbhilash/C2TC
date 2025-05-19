f5ee5047cc0a4b449d12f0f4aab67ae32341d89b
Alphones <alphonesabhilash@gmail.com>
2025-05-20 00:58:04 +0530

    Generics in Java

---
+++ TNS_JAVA/src/com/tns/generics/Student.java
@@ -0,0 +1,16 @@
+package com.tns.generics;
+
+public class Student {
+	int rollno;
+	float marks;
+	String name;
+	
+	public Student(int rno,float marks,String name) {
+		this.rollno=rno;
+		this.marks=marks;
+		this.name=name;
+	}
+	
+	
+}
+	
